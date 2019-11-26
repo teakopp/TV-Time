f3d10311cf54be50afb923c736eeb3a1db5897fc
Travis Kopp <teakopp@github.com>
2019-11-25 17:36:05 -0800

    set IT scheduler

--- renderer.js
+++ renderer.js
@@ -5,8 +5,8 @@ const path = require('path')
 //Creates TV class for your viewing pleasure
 class TV {
 
-  constructor(directory, htmlIdTag){
-    this.directory = path.resolve(__dirname, directory )
+  constructor(directory, htmlIdTag) {
+    this.directory = path.resolve(__dirname, directory)
     this.index = 0;
     this.length = 0;
     this.images = [];
@@ -14,55 +14,14 @@ class TV {
     this.standByDisplayed = false
   }
 
-// Recursive loop to get a different random number, if same one picked twice
-// then sets it when unique number found
-  changeToRandom(){
-    let random = this.randomPick(this.length - 1)
-    console.log(this.index);
-    if (this.index == random){
-      this.changeToRandom()
-    }
-    else {
-      this.index = random
-      let image = this.images[this.index]
-      this.setChannel(image, this.directory)
-    }
-  }
 
-  // Increments image up one in the array of files
-  // then sets it
-  changeUp(){
-    this.index += 1
-    if (this.index > this.length){
-      this.index = 0
-    }
-    let image = this.images[this.index]
-    this.setChannel(image, this.directory)
-  }
-
-// Increments image down one in the array of files
-// then sets it
-  changeDown(){
-    this.index -= 1
-    console.log(this.index);
-    if (this.index <= 0){
-      this.index = this.length
-    }
-    this.setChannel(this.images[this.index], this.directory)
-  }
-
-// Gets a random number
-  randomPick(max){
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
-// Goes through this.directory and gets all files out of it.
-// I haven't tested putting non-images so proceed with caution
-// Also beware of callback hell when messing with it
-  getFiles(){
+  // Goes through this.directory and gets all files out of it.
+  // I haven't tested putting non-images so proceed with caution
+  // Also beware of callback hell when messing with it
+  getFiles() {
     var things = []
 
-    fs.readdir(this.directory , (err, files) => {
+    fs.readdir(this.directory, (err, files) => {
       if (err) {
         console.log(err)
       }
@@ -74,53 +33,53 @@ class TV {
 
       this.images = this.callback(things)
       this.length = this.images.length - 1
-      this.changeToRandom()
+      console.log(this.images);
+      this.setChannelBasedOnHelpdeskSchedule()
     })
   }
 
-// generic callback function, I use this mostly for logging tbh
-  callback(value){
+  // generic callback function, I use this mostly for logging tbh
+  callback(value) {
     return value
   }
 
-// Use this function to set display the new image after using other functions to make changes
-  setChannel(image, directory){
-    let new_image = path.join(directory +'/' + image)
+  // Use this function to set display the new image after using other functions to make changes
+  setChannel(image, directory) {
+    let new_image = path.join(directory + '/' + image)
     console.log(new_image);
     var img = document.getElementById(this.htmlIdTag);
     img.src = new_image;
   }
 
-  isImageHidden(){
+  isImageHidden() {
     var img = document.getElementById(this.htmlIdTag);
-     if(img.style.visibility == 'hidden'){
-       return true
-     }
-     else{
-       return false
-     }
+    if (img.style.visibility == 'hidden') {
+      return true
+    } else {
+      return false
+    }
   }
 
-  hideImage(){
+  hideImage() {
     var img = document.getElementById(this.htmlIdTag);
     img.style.visibility = 'hidden'
   }
-  showImage(){
+  showImage() {
     var img = document.getElementById(this.htmlIdTag);
     img.style.visibility = 'visible'
   }
-  displayPleaseStandByScreen(){
+  displayPleaseStandByScreen() {
     let image = 'giphy-2.gif'
     let directory = __dirname + '/default-images/'
     this.setChannel(image, directory)
     this.standByDisplayed = true
   }
-  hideDisplayPleaseStandByScreen(){
+  hideDisplayPleaseStandByScreen() {
     this.setChannel(this.images[this.index], this.directory)
     this.standByDisplayed = false
   }
 
-  addStatic(){
+  addStatic() {
     // this.displayPleaseStandByScreen()
     setTimeout(() => {
 
@@ -133,50 +92,62 @@ class TV {
     }, 10)
   }
 
-  automaticallyChangeChannel(){
+  refreshChannel() {
     setTimeout(() => {
-      this.addStatic()
-      this.changeToRandom()
-      this.automaticallyChangeChannel()
+      this.setChannelBasedOnHelpdeskSchedule()
+      this.refreshChannel()
       return
     }, 1800000)
+  }
+
+  getDayOfTheWeek(){
+    var date = new Date();
+    var number = date.getDay();
+    return date.getDay();
+  }
+
+  getCurrentTime(){
+    let date = new Date();
+    return date.getHours()
+  }
+
+  getImageByDayOfWeek(){
+    let day = this.getDayOfTheWeek()
+    if(day >= 1 && day <= 3 ){
+      return ("IT.gif")
+    }
+    else if(day == 4 || day == 5){
+      return ("IT-Hubot-Gif-off-thurs-fri.gif")
+    }
+    else{
+      return ("IT-Hubot-Gif-closed.gif")
+    }
+  }
+
+  helpdeskIsOpen(){
+    let currentHour = this.getCurrentTime()
+
+    if(currentHour < 17 && currentHour >=  9){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+  setChannelBasedOnHelpdeskSchedule(){
+    let helpDeskIsOpen = this.helpdeskIsOpen()
+    if(helpDeskIsOpen){
+      televison.setChannel(televison.getImageByDayOfWeek(), "gifs")
+    }
+    else{
+      televison.setChannel("IT-Hubot-Gif-closed.gif", "gifs")
+    }
   }
 
 }
 
 let televison = new TV("gifs", "img-main")
 televison.getFiles()
-televison.automaticallyChangeChannel()
-
-// Keeping this out of function, because I'm scared of weirdness it could potentially cause
-document.onkeydown = (e) => {
-    switch (e.keyCode) {
-      // Esc Key
-      case 27:
-      if(televison.standByDisplayed){
-        televison.hideDisplayPleaseStandByScreen()
-      }
-      else{
-        televison.displayPleaseStandByScreen()
-      }
-      break;
-
-      // Spacebar
-      case 32:
-        televison.addStatic()
-        televison.changeToRandom()
-        break;
-
-      // Left Arrow
-      case 37:
-        televison.addStatic()
-        televison.changeDown()
-        break;
-
-      // Right Arrow
-      case 39:
-        televison.addStatic()
-        televison.changeUp()
-        break;
-    }
-};
+televison.setChannelBasedOnHelpdeskSchedule()
+televison.refreshChannel()
